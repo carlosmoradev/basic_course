6a54f33fd8d02d2a70ba16c4a805a4fb1720430f
Carlos Mario Mora Restrepo <CarlosMora@SRE-MacBook-Pro.local>
2022-04-26 09:59:27 -0500

    starting again with python

---
+++ conversor.py
@@ -0,0 +1,8 @@
+#Conversor de monedas entre pesos colobianos y dolares
+
+pesos = input('Cuantos pesos colombianos tienes?: ')
+pesos = float(pesos)
+valor_dolar = 3931.74
+dolares = pesos / valor_dolar
+
+print(f"{pesos} pesos equivale a {dolares:.2f} dolares.") # el :.2f redondea la cantidad de decimales a 2
