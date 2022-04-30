4b930d474e582eb6a0eba862029d977f831f8181
Carlos M. Mora <git@carlosmora.dev>
2022-04-30 06:46:16 -0500

    README agregado

---
+++ README.md
@@ -0,0 +1,3 @@
+###Python basic course
+
+Este es el resumen de los ejercicios del curso basico de Python ofrecido por Platzi.
---
+++ diccionario.py
@@ -0,0 +1,16 @@
+def run():
+    poblacion_paises = {
+        "Argentina": 60449332,
+        "Colombia": 54440233,
+        "Brasil": 90459403,
+        "Bolivia": 30292340
+    }
+
+
+    print(poblacion_paises["Bolivia"])
+
+
+
+
+if __name__ == '__main__':
+    run()
